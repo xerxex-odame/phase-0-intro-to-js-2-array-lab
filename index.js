d0b25d9b5ffd78ac993575cd271a1c27414ea504
xerxex-odame <kingodame@yahoo.com>
2022-05-14 12:50:01 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+  ];
+  
+  function destructivelyAppendCat(name){
+    return cats.push(name);
+  }
+  
+  function destructivelyPrependCat(name){
+    return cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat(){
+    return cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat(){
+    return cats.shift();
+  }
+  
+  function appendCat(name){
+    return [...cats, name];
+  }
+  
+  function prependCat(name){
+    return [name,...cats];
+  };
+  
+  //function removeLa
